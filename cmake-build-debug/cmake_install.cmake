f8f5811d6d0a30895bb27d2fd3ae861bc8b4b9aa
Revenant <129858973+Revenant121@users.noreply.github.com>
2024-05-15 16:35:56 +0300

    practica11

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Лиджик/CLionProjects/practica9
+# Install script for directory: C:/Users/Лиджик/CLionProjects/practica11
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/practica9")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/practica11")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Лиджик/CLionProjects/practica9/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Лиджик/CLionProjects/practica11/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -1,184 +1,140 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-#define SIZE 5
-#define ROWS 9
-#define COLS 5
+#define MAX_NAME_LENGTH 50
 
-void set_positive_to_zero(int numbers[], int size);
-void initialize_matrix(int matrix[][COLS], int rows, int cols);
-void count_zeros_in_third_rows(int matrix[][COLS], int rows, int cols);
-void checkSegmentsIntersection();
-int* input_numbers(int n);
-void print_squares(int *numbers, int n);
-void free_memory(int *numbers);
+struct Student {
+    char surname[MAX_NAME_LENGTH];
+    char name[MAX_NAME_LENGTH];
+    char gender;
+    int age;
+    char group[MAX_NAME_LENGTH];
+    int math_grade;
+    int physics_grade;
+    int chemistry_grade;
+};
 
-typedef int (*CalculatorFunction)(int, int);
+struct TreeNode {
+    struct Student* student;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
 
-int add(int a, int b) { return a + b; }
-int subtract(int a, int b) { return a - b; }
-int multiply(int a, int b) { return a * b; }
-int divide(int a, int b) { return a / b; }
+struct ContainerNode {
+    struct Student students[2];
+    int size;
+    struct ContainerNode* next;
+};
+
+void increase_physics_grade(struct Student *student) {
+    if (student->physics_grade < 5) {
+        student->physics_grade++;
+    }
+}
+
+void print_student_info(struct Student *student) {
+    printf("Surname: %s\n", student->surname);
+    printf("Name: %s\n", student->name);
+    printf("Gender: %c\n", student->gender);
+    printf("Age: %d\n", student->age);
+    printf("Group: %s\n", student->group);
+    printf("Math grade: %d\n", student->math_grade);
+    printf("Physics grade: %d\n", student->physics_grade);
+    printf("Chemistry grade: %d\n", student->chemistry_grade);
+}
+
+void generate_random_grades(struct Student *student) {
+    student->math_grade = rand() % 5 + 1;
+    student->physics_grade = rand() % 5 + 1;
+    student->chemistry_grade = rand() % 5 + 1;
+}
+
+struct TreeNode* insert_student(struct TreeNode* root, struct Student* student) {
+    if (root == NULL) {
+        struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+        newNode->student = student;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        return newNode;
+    }
+
+    if (strcmp(student->surname, root->student->surname) < 0) {
+        root->left = insert_student(root->left, student);
+    } else {
+        root->right = insert_student(root->right, student);
+    }
+
+    return root;
+}
+
+void print_students_in_order(struct TreeNode* root) {
+    if (root != NULL) {
+        print_students_in_order(root->right);
+        print_student_info(root->student);
+        printf("\n");
+        print_students_in_order(root->left);
+    }
+}
+
+void add_students_to_tree(struct TreeNode** root, struct ContainerNode* container) {
+    while (container != NULL) {
+        for (int i = 0; i < container->size; i++) {
+            struct Student* student = &(container->students[i]);
+            *root = insert_student(*root, student);
+        }
+        container = container->next;
+    }
+}
 
 int main() {
-    int numbers[SIZE];
-    srand(time(0));
+    srand(time(NULL));
 
-    printf("Random numbers:\n");
-    for (int i = 0; i < SIZE; ++i) {
-        numbers[i] = rand() % (100 - (-100) + 1) + (-100);
-        printf("%d ", numbers[i]);
+    struct ContainerNode* head = NULL;
+    struct ContainerNode* current = NULL;
+
+    for (char group = 'A'; group <= 'C'; group++) {
+        struct ContainerNode* newContainer = (struct ContainerNode*)malloc(sizeof(struct ContainerNode));
+        newContainer->size = 2;
+
+        for (int i = 0; i < newContainer->size; i++) {
+            struct Student* student = &(newContainer->students[i]);
+            snprintf(student->group, sizeof(student->group), "Group %c", group);
+            snprintf(student->surname, sizeof(student->surname), "Surname_%c_%d", group, i+1);
+            snprintf(student->name, sizeof(student->name), "Name_%c_%d", group, i+1);
+            student->gender = (i % 2 == 0) ? 'M' : 'F';
+            student->age = 18 + (rand() % 5);
+            generate_random_grades(student);
+        }
+
+        newContainer->next = NULL;
+        if (head == NULL) {
+            head = newContainer;
+            current = head;
+        } else {
+            current->next = newContainer;
+            current = current->next;
+        }
     }
-    printf("\n");
 
-    void (*set_positive_to_zero_ptr)(int[], int) = &set_positive_to_zero;
-    void (*initialize_matrix_ptr)(int[][COLS], int, int) = &initialize_matrix;
-    void (*count_zeros_in_third_rows_ptr)(int[][COLS], int, int) = &count_zeros_in_third_rows;
-    void (*checkSegmentsIntersection_ptr)() = &checkSegmentsIntersection;
-    int* (*input_numbers_ptr)(int) = &input_numbers;
-    void (*print_squares_ptr)(int*, int) = &print_squares;
-    void (*free_memory_ptr)(int*) = &free_memory;
+    struct TreeNode* root = NULL;
 
-    set_positive_to_zero_ptr(numbers, SIZE);
+    add_students_to_tree(&root, head);
 
-    printf("\nResult:\n");
-    for (int i = 0; i < SIZE; ++i) {
-        printf("%d ", numbers[i]);
-    }
-    printf("\n\n");
+    printf("Student information with initial grades:\n");
+    print_students_in_order(root);
 
-    int matrix[ROWS][COLS];
-    initialize_matrix_ptr(matrix, ROWS, COLS);
-    count_zeros_in_third_rows_ptr(matrix, ROWS, COLS);
+    increase_physics_grade(root->student);
 
-    int n;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
+    printf("\nStudent information after increasing physics grade:\n");
+    print_students_in_order(root);
 
-    int *dynamic_numbers = input_numbers_ptr(n);
-    print_squares_ptr(dynamic_numbers, n);
-    free_memory_ptr(dynamic_numbers);
-
-    checkSegmentsIntersection_ptr();
-
-    CalculatorFunction calculator[4] = {&add, &subtract, &multiply, &divide};
-
-    int a, b;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-
-    for (int i = 0; i < 4; ++i) {
-        printf("Result of operation %d: %d\n", i + 1, calculator[i](a, b));
+    while (head != NULL) {
+        struct ContainerNode* temp = head;
+        head = head->next;
+        free(temp);
     }
 
     return 0;
-}
-
-void set_positive_to_zero(int numbers[], int size) {
-    for (int i = 0; i < size; ++i) {
-        if (numbers[i] > 0) {
-            numbers[i] = 0;
-        }
-    }
-}
-
-void initialize_matrix(int matrix[][COLS], int rows, int cols) {
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            matrix[i][j] = 0;
-        }
-    }
-}
-
-
-void count_zeros_in_third_rows(int matrix[][COLS], int rows, int cols);
-void (*fsdad)(int [][COLS],int,int ) = count_zeros_in_third_rows;
-
-
-{
-    for (int i = 2; i < rows; i += 3) {
-        int count_zeros = 0;
-        for (int j = 0; j < cols; ++j) {
-            if (matrix[i][j] == 0) {
-                count_zeros++;
-            }
-        }
-        printf("Number of zeros in %d row: %d\n", i + 1, count_zeros);
-    }
-}
-
-void checkSegmentsIntersection() {
-    float x1, x2, x3, x4, y1, y2, y3, y4, k1, k2, b1, b2, x_intersect, y_intersect;
-
-    printf("Enter the coordinates of point A of segment 1: ");
-    printf("Enter x: ");
-    scanf("%f", &x1);
-    printf("Enter y: ");
-    scanf("%f", &y1);
-
-    printf("Enter the coordinates of point B of segment 1: ");
-    printf("Enter x: ");
-    scanf("%f", &x2);
-    printf("Enter y: ");
-    scanf("%f", &y2);
-
-    printf("Enter coordinates of point A1 of segment 2: ");
-    printf("Enter x: ");
-    scanf("%f", &x3);
-    printf("Enter y: ");
-    scanf("%f", &y3);
-
-    printf("Enter the coordinates of point B2 of segment 2: ");
-    printf("Enter x: ");
-    scanf("%f", &x4);
-    printf("Enter y: ");
-    scanf("%f", &y4);
-
-    k1 = (y2 - y1) / (x2 - x1);
-    b1 = y1 - (k1 * x1);
-    k2 = (y3 - y4) / (x4 - x3);
-    b2 = y3 - (k2 * x3);
-
-    if (k1 == k2) {
-        if (b1 == b2)
-            printf("The segments match \n");
-        else
-            printf("The segments are parallel and do not intersect \n");
-    } else {
-        x_intersect = (b2 - b1) / (k1 - k2);
-        y_intersect = k1 * x_intersect + b1;
-
-        if (((x1 <= x4) || (x4 <= x2)) || ((x1 <= x3) || (x3 <= x2)))
-            printf("The segments intersect \n");
-        else
-            printf("The segments do not intersect \n");
-    }
-}
-
-int* input_numbers(int n) {
-    int *numbers = (int *)malloc(n * sizeof(int));
-    if (numbers == NULL) {
-        printf("Memory allocation error.\n");
-        exit(1);
-    }
-
-    printf("Enter %d numbers:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &numbers[i]);
-    }
-
-    return numbers;
-}
-
-void print_squares(int *numbers, int n) {
-    printf("Squares of the entered numbers:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", numbers[i] * numbers[i]);
-    }
-    printf("\n");
-}
-
-void free_memory(int *numbers) {
-    free(numbers);
 }
